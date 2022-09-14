49106b9988e9fa2e08f2649c198d087603e8e85c
kaechesbord <damirlihtneri6@gmail.com>
2022-09-14 14:37:55 +0200

    drugi commit ali jos uvijek problem

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,14 @@
+import { useDispatch, useSelector } from "react-redux/es/exports"
+import { addUser } from "./User";
+
 const Form = () => {
+    //const user = useSelector((state) => state.user.user )
+    const dispatch = useDispatch();
   return (
     <div>
       <input type="text" placeholder='First name'/>
       <input type="text" placeholder='Last name'/>
-      <button>SUBMIT</button>
+      <button onClick={() => dispatch(addUser())}>SUBMIT</button>
     </div>
   )
 }
